840068b40388e8aed051359140df0b2212491e73
Jameson <jamesonarnett@yahoo.com>
2022-04-06 17:34:59 -0500

    Adds dummy data for shopping items // init StartScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
+import HomeScreen from "./screens/HomeScreen";
 import Footer from "./components/Footer";
 
 const App = () => {
@@ -8,7 +9,7 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-          <h1>Welcome to shopMe</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
