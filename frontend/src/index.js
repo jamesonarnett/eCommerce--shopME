83e80c5b422b27b7fbbb9767831d606152a99031
jamesonarnett <jamesonarnett@yahoo.com>
2022-04-11 14:42:16 -0500

    Adds mongoDB config

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,14 +4,6 @@ import "./bootstrap.min.css";
 import "./index.css";
 import App from "./App";
 
-// function AppWithCallbackAfterRender() {
-//   useEffect(() => {
-//     console.log('rendered');
-//   });
-
-//   return <App tab="home" />
-// }
-
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
 root.render(<App />);
