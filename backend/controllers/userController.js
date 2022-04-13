5e5f736b265f48bdd2ab80dde1419d7c6aa6b0f4
jamesonarnett <jamesonarnett@yahoo.com>
2022-04-13 17:54:14 -0500

    Adds user registration && salt

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,6 +4,7 @@ import generateToken from "../utils/generateToken.js";
 
 // Auth user && token
 // POST /api/users/login
+//public
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
@@ -20,6 +21,39 @@ const authUser = asyncHandler(async (req, res) => {
   } else {
     res.status(401);
     throw new Error("Invalid Email or Password");
+  }
+});
+
+// Register new user
+// POST /api/users/
+//public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email: email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
   }
 });
 
@@ -43,4 +77,4 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, getUserProfile };
+export { authUser, registerUser, getUserProfile };
